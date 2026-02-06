afc93498ab369b7e370ed287d911416c48608bc4
iwat <18253321614@163.com>
2026-02-07 00:19:21 +0800

    Site updated: 2026-02-07 00:19:21

--- js/third-party/comments/utterances.js
+++ js/third-party/comments/utterances.js
@@ -3,15 +3,20 @@
 document.addEventListener('page:loaded', async () => {
   if (!CONFIG.page.comments) return;
 
+  const { repo, issue_term, label, theme, lang } = CONFIG.utterances;
+  const attributes = {
+    async       : true,
+    crossOrigin : 'anonymous',
+    'repo'      : repo,
+    'issue-term': issue_term,
+    'theme'     : theme
+  };
+  if (label) attributes.label = label;
+  if (lang) attributes.lang = lang;
+
   await NexT.utils.loadComments('.utterances-container');
   await NexT.utils.getScript('https://utteranc.es/client.js', {
-    attributes: {
-      async       : true,
-      crossOrigin : 'anonymous',
-      'repo'      : CONFIG.utterances.repo,
-      'issue-term': CONFIG.utterances.issue_term,
-      'theme'     : CONFIG.utterances.theme
-    },
+    attributes,
     parentNode: document.querySelector('.utterances-container')
   });
 });
